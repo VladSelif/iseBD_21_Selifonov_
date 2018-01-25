93dc8215e7f22215522ed0107b5bd304b21e3968
VladSelif <31828191+VladSelif@users.noreply.github.com>
2018-01-25 13:02:11 +0300

    Add files via upload

---
+++ Airplane.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  Laba_2__samolet_
+{
+    public class Airplane : Vehicle
+    {
+        public override int MaxSpeed
+        {
+            get
+            {
+                return base.MaxSpeed;
+            }
+            protected set
+            {
+                if (value > 0 && value < 1300)
+                {
+                    base.MaxSpeed = value;
+                }
+                else
+                {
+                    base.MaxSpeed = 1150;
+                }
+            }
+        }
+
+        public override int MaxCountPassengers
+        {
+            get
+            {
+                return base.MaxCountPassengers;
+            }
+            protected set
+            {
+                if (value > 0 && value < 5)
+                {
+                    base.MaxCountPassengers = value;
+                }
+                else
+                {
+                    base.MaxCountPassengers = 4;
+                }
+            }
+        }
+
+        public override double Weight
+        {
+            get
+            {
+                return base.Weight;
+            }
+            protected set
+            {
+                if (value > 1500 && value < 2500)
+                {
+                    base.Weight = value;
+                }
+                else
+                {
+                    base.Weight = 2000;
+                }
+            }
+        }
+
+        public Airplane(int maxSpeed, int maxCountPassenger, double weight, Color color)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.MaxCountPassengers = maxCountPassenger;
+            this.ColorBody = color;
+            this.Weight = weight;
+            this.countPassengers = 0;
+            Random rand = new Random();
+            startPosX = rand.Next(10, 200);
+            startPosY = rand.Next(10, 200);
+        }
+
+        public override void moveAirplane(Graphics g)
+        {
+            startPosX += (MaxSpeed * 50 / (float)Weight) / (countPassengers == 0 ? 1 : countPassengers);
+            drawAirplane(g);
+        }
+
+        public override void UpAirplane(Graphics g)
+        {
+            startPosY -= (MaxSpeed * 50 / (float)Weight) / (countPassengers == 0 ? 1 : countPassengers);
+            drawAirplane(g);
+        }
+
+        public override void DownAirplane(Graphics gr)
+         {
+            startPosY += (MaxSpeed * 50 / (float)Weight) / (countPassengers == 0 ? 1 : countPassengers);
+            drawAirplane(gr);
+         }
+
+    public override void drawAirplane(Graphics g)
+        {
+            drawLightAirplane(g);
+        }
+
+        protected virtual void drawLightAirplane(Graphics g)
+        {       
+            Brush br = new SolidBrush(ColorBody);
+            g.FillRectangle(br, startPosX + 10, startPosY+15 , 10, 40);
+            g.FillRectangle(br, startPosX+35, startPosY-10 ,15, 90);    
+            g.FillRectangle(br, startPosX , startPosY + 25, 66, 20);
+            g.FillRectangle(br, startPosX+ 76, startPosY + 25, 4, 20);
+            Brush brBlack = new SolidBrush(Color.Black);            
+            g.FillRectangle(brBlack, startPosX+66, startPosY + 25, 10, 20);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_2__samolet_
+{
+    public partial class Form1 : Form
+    {
+        Color color;
+        Color dopColor;
+        int maxSpeed;
+        int maxCountPass;
+        int weight;
+
+        private ITransport inter;
+        public Form1()
+
+        {
+            InitializeComponent();
+            color = Color.Gray;
+            dopColor = Color.LightBlue;
+            maxSpeed = 150;
+            maxCountPass = 4;
+            weight = 1500;
+            SelectColor.BackColor = color;
+            SelectDopColor.BackColor = dopColor;
+        }
+
+        private void MoveAirplane_Click(object sender, EventArgs e)
+        {
+            if (inter != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.moveAirplane(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void zadatsamolet_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+
+                inter = new Airplane(maxSpeed, maxCountPass, weight, color);
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.drawAirplane(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void SelectColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                color = cd.Color;
+                SelectColor.BackColor = color;
+            }
+        }
+
+        private bool checkFields()
+        {
+            if (!int.TryParse(textBoxmaxspeed.Text, out maxSpeed))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBoxcountpass.Text, out maxCountPass))
+            {
+                return false;
+            }
+            if (!int.TryParse(textBoxWeight.Text, out weight))
+            {
+                return false;
+            }
+            return true;
+        }
+        private void zadatAerobus_Click(object sender, EventArgs e)
+        {
+            if (checkFields())
+            {
+
+                if (checkFields())
+                {
+                    inter = new Aerobus(maxSpeed, maxCountPass, weight, color, windows.Checked, reactiveEngine_left.Checked,reactiveEngine_right.Checked, lines.Checked, dopColor);
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    Graphics gr = Graphics.FromImage(bmp);
+                    inter.drawAirplane(gr);
+                    pictureBox1.Image = bmp;
+                }
+            }
+        }
+
+        private void SelectDopColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dopColor = cd.Color;
+                SelectDopColor.BackColor = dopColor;
+            }
+        }
+
+        private void Take_off_Click(object sender, EventArgs e)
+        {
+            if (inter != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.UpAirplane(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void DownAirplane_Click(object sender, EventArgs e)
+        {
+            if (inter != null)
+            {
+                Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                inter.DownAirplane(gr);
+                pictureBox1.Image = bmp;
+            }
+        }
+    }
+}
